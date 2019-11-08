1ad4f4958b508d8e74ba2c76b883f645fdca7f50
Kimberly Conkle <kimberlyconkle88@gmail.com>
2019-11-08 15:50:52 +0000

    j

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ApplicationRecord
     belongs_to :user
     belongs_to :places
-
-    
+  end
+  
+  class Photo < ActiveRecord::Base
+    mount_uploader :picture, PictureUploader
   end
