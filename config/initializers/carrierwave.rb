fcc98ee87dd5adce2c0ae8e18b2c64d9410ff61f
Kimberly Conkle <kimberlyconkle88@gmail.com>
2019-11-09 20:24:45 +0000

    jjj

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'                        # required
-    config.fog_public = false
-    config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     ENV["AKIAQ64XMQROL3PGRT6Z"],        # required
-      aws_secret_access_key: ENV["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"],        # required
-    }
-    config.fog_directory  = ENV["nomsterkimconkle"]              # required
-  end
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_public = false
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AKIAQ64XMQROL3PGRT6Z"],        # required
+    aws_secret_access_key: ENV["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"],        # required
+  }
+  config.fog_directory  = ENV["nomsterkimconkle"]              # required
+end
